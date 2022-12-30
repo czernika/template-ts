0eecb4590701177bb6622ebc02d37eb5709e2d9a
Aliakseyenka Ihar <aliha.devs@gmail.com>
2022-12-30 18:46:01 +0300

    refactor: empty declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-declare module 'wordpress-wolat' {
-    import { Plugin } from 'vite'
-    const plugin: Plugin
-    export default plugin
-}
+// declare module 'package-name' {
+//     import { Plugin } from 'vite'
+//     const plugin: Plugin
+//     export default plugin
+// }
